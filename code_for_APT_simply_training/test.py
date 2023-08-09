3bd0bfe8a4d4d10d72dd4ef9a63d44f616d7249c
xsy786912649 <786912649@qq.com>
2023-08-08 20:43:18 -0400

    update

--- code_for_APT_simply_training/test.py
+++
@@ -1,7 +0,0 @@
-from pomdp import *
-from model import *
-
-#['comp145', 'comp361', 'comp499', 'comp199', 'comp246', 'comp398', 'comp252', 'comp102', 'comp296', 'comp442', 'comp155', 'comp347', 'comp427', 'comp320']
-
-
-
